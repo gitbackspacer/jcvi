a3a0b086a0b5defa9bd1180a22110e75bf33336a
gitbackspacer <49907563+gitbackspacer@users.noreply.github.com>
2020-10-13 11:20:41 +0100

    label rotation set 90 degree

--- jcvi/graphics/synteny.py
+++ jcvi/graphics/synteny.py
@@ -247,7 +247,7 @@ class Region(object):
                     y + height / 2 + genelabelsize * vpad / 3,
                     markup(g.accn),
                     size=genelabelsize,
-                    rotation=25,
+                    rotation=90,
                     ha="left",
                     va="center",
                     color="lightslategray",
